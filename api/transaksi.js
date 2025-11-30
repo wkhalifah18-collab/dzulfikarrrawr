6dfa6f185f2b07d02b52f51abb453c43475f6054
wkhalifah18-collab <wkhalifah18@gmail.com>
2025-11-30 14:42:50 +0900

    Refactor API handler to require POST method

--- api/transaksi.js
+++ api/transaksi.js
@@ -1,3 +1,7 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "API OK" });
-}
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(200).json({ message: "API transaksi jalan (POST required)" });
+  }
+
+  return res.status(200).json({ message: "POST berhasil diterima" });
+};
