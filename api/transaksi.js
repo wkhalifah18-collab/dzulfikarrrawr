3ffcd1085853d222eaffe0d7d5b213618272094c
wkhalifah18-collab <wkhalifah18@gmail.com>
2025-11-30 15:05:51 +0900

    Delete api/transaksi.js

--- api/transaksi.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(200).json({ message: "API transaksi jalan (POST required)" });
-  }
-
-  return res.status(200).json({ message: "POST berhasil diterima" });
-};
